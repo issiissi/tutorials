479d8c3eb23dbc1343321832ed707c67552fb608
Isabell <igr45225@stud.hs-furtwangen.de>
2025-10-13 17:14:05 +0200

    Ende Übung 2

--- tutorials/src/02-pixel-manipulation.problem.py
+++ tutorials/src/02-pixel-manipulation.problem.py
@@ -7,36 +7,51 @@
 import cv2
 import numpy as np # we will need numpy for some array manipulations
 
-# TODO Loading images in grey and color and store them in the variables 'img_gray' and 'img_color'.
+# Loading images in grey and color and store them in the variables 'img_gray' and 'img_color'.
+img_gray=cv2.imread("tutorials/data/images/logo.png",cv2.IMREAD_GRAYSCALE)
+img_color=cv2.imread("tutorials/data/images/logo.png",cv2.IMREAD_COLOR)
 
-# TODO Check if images have been loaded successfully.
-
+# Check if images have been loaded successfully.
+if img_gray is None:
+    raise FileNotFoundError("Image not found or failed to load.")
+if img_color is None:
+    raise FileNotFoundError("Image not found or failed to load.")
 ## Image data access
 
-# TODO Do some print out about the loaded data using 'type', 'dtype' and 'shape'.
+# Do some print out about the loaded data using 'type', 'dtype' and 'shape'.
+print(type(img_gray))  
+print(type(img_color))
+print(img_gray.dtype)
+print(img_color.dtype)
+print(img_gray.shape)
+print(img_color.shape)
 
-# TODO Continue with the grayscale image by copying it to a new variable 'img'.
-
-# TODO Extract the size or resolution of this image.
-
+#  Continue with the grayscale image by copying it to a new variable 'img'.
+img=img_gray.copy()
+#  Extract the size or resolution of this image.
+height, width =img.shape
+##bgr automatisch eingestellte farbwerte bei bild
 ## This is how you can access the height and width of an image stored as a numpy ndarray. 
 ## In all following exercises you should use this way to access the height and width of an image.
 ## Then, you can use the variables 'height' and 'width' in your code instead of hardcoding values. 
 ## This makes your code flexible and independent from the actual image size.
 
-# TODO Resize the image to a small size (7x5) as we will later print out the pixel values.
-
+#  Resize the image to a small size (7x5) as we will later print out the pixel values.
+img= cv2.resize(img_color, (7,5))
 # HINT: Row and column access, see https://numpy.org/doc/stable/reference/arrays.ndarray.html for 
 # general access on ndarrays
 
-# TODO Print first row
+#  Print first row
+print(img[0])
+#  Print first column
+print(img[:,0])
 
-# TODO Print first column
-
-# TODO Now continue with the color image by copying it to the variable 'img'.
-
+#  Now continue with the color image by copying it to the variable 'img'.
+img=img_color.copy()
 # TODO Set an area of the image to black by looping over the pixels and setting the pixel values to [0,0,0].
-
+for i in range(0,10):
+    for j in range (0,10):
+       img[i,j:]=[0,0,0]
 # TODO Create a window with 'namedWindow' and show the image and wait until key pressed.
 
 # TODO Find all used colors in the image by first reshaping the image to a list of pixels with 'reshape' 
--- tutorials/src/03-video-cam.problem.py
+++ tutorials/src/03-video-cam.problem.py
@@ -6,31 +6,42 @@
 import numpy as np
 import cv2
 
-# TODO Capture webcam image using 'VideoCapture' and store it in the variable 'cap'. Typically, 
+#  Capture webcam image using 'VideoCapture' and store it in the variable 'cap'. Typically, 
 # the parameter is 0 for the default camera.
-
-# TODO Get camera image parameters using 'get' on the 'cap' object:
+cap=cv2.VideoCapture(0)
+#  Get camera image parameters using 'get' on the 'cap' object:
 # - width (use 'CAP_PROP_FRAME_WIDTH')
 # - height (use 'CAP_PROP_FRAME_HEIGHT')
 # - codec (use 'CAP_PROP_CODEC_PIXEL_FORMAT')
 # Print the values to the console.
-
-# TODO Create a window for the video using 'namedWindow' with the title "Video image".
-
-# TODO Start a loop to continuously read frames from the camera using 'while True:'.
-
-# TODO (In loop) Read a camera frame with 'read' and check if that was successful.
-
-# TODO (In loop, if successful) (Skip this step for now and implement the display first in order 
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#  Create a window for the video using 'namedWindow' with the title "Video image".
+title="my webcam"
+cv2.namedWindow(title, cv2.WINDOW_NORMAL)
+#  Start a loop to continuously read frames from the camera using 'while True:'.
+while True: 
+#  (In loop) Read a camera frame with 'read' and check if that was successful.
+ ret, frame=cap.read()
+#  (In loop, if successful) (Skip this step for now and implement the display first in order 
+ if ret: 
+    
 # to see the camera image.)
 # Create four flipped tiles of the image by first creating an empty image 
 # with 'np.zeros'. Then resize the frame to half size using 'cv2.resize' with fx=0.5 and fy=0.5.
 # Finally, fill in the four quadrants of the empty image with the original and flipped images
 # using 'cv2.flip' with the appropriate flip codes (check the documentation).
-
-# TODO (In loop, if successful) Display the image.
-
-# TODO (In loop) Check if 'q' is pressed by comparing the return value of 'waitKey' with the 'ord'
+ img =np.zeros.like(frame)
+ tile= cv2.resize(frame, (0,0), fx=0.5,  fy=0.5)
+ img[: title.shape[0], 0: title.shape[1]]= title
+#  (In loop, if successful) Display the image.
+ cv2.imshow(title,frame)
+ key=cv2.waitKey(20)
+ if key==ord('q'):
+    break
+#  (In loop) Check if 'q' is pressed by comparing the return value of 'waitKey' with the 'ord'
 # of the letter "q". Use simply 'break' to exit the loop.
 
-# TODO Release the video capture object with 'release' and close the window with 'destroyAllWindows'.
+# Release the video capture object with 'release' and close the window with 'destroyAllWindows'.
+cap.release()
+cv2.destroyAllWindows()
